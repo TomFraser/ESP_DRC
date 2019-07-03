7df7d00f6157e1bcbf7bf01e435646901e7d1898
Tom Hulbert <18743@student.bbc.qld.edu.au>
2019-07-03 12:00:39 +1000

    mega hacky wifi server stuff

--- libs/ap_server/include/ap_server.c
+++ libs/ap_server/include/ap_server.c
@@ -19,18 +19,213 @@
 
 #include "../../ESP_common.h"
 
+#include "protocol_examples_common.h"
+#include "sdkconfig.h"
+#include "esp_eth.h"
+
+#define ssid "WACT^2 Router"
+#define pass "YeetYeet"
+
+#define GOT_IPV4_BIT BIT(0)
+#define GOT_IPV6_BIT BIT(1)
+
+#ifdef CONFIG_EXAMPLE_CONNECT_IPV6
+#define CONNECTED_BITS (GOT_IPV4_BIT | GOT_IPV6_BIT)
+#else
+#define CONNECTED_BITS (GOT_IPV4_BIT)
+#endif
+
+static EventGroupHandle_t s_connect_event_group;
+static ip4_addr_t s_ip_addr;
+static const char *s_connection_name;
+
+#ifdef CONFIG_EXAMPLE_CONNECT_IPV6
+static ip6_addr_t s_ipv6_addr;
+#endif
+
 static const char *TAG = "http_server";
-
-#define ESP_WIFI_SSID      "WACT² Robot"
-#define ESP_WIFI_PASS      "YeetYeet"
-#define MAX_STA_CONN       5
-
-#define NUMBER_OF_STRING 20
-#define MAX_STRING_SIZE 50
 
 static EventGroupHandle_t wifi_event_group;
 
 static robot_t * robot;
+
+/* set up connection, Wi-Fi or Ethernet */
+static void start();
+
+/* tear down connection, release resources */
+static void stop();
+
+static void on_got_ip(void *arg, esp_event_base_t event_base,
+                      int32_t event_id, void *event_data)
+{
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+    memcpy(&s_ip_addr, &event->ip_info.ip, sizeof(s_ip_addr));
+    xEventGroupSetBits(s_connect_event_group, GOT_IPV4_BIT);
+}
+
+#ifdef CONFIG_EXAMPLE_CONNECT_IPV6
+
+static void on_got_ipv6(void *arg, esp_event_base_t event_base,
+                        int32_t event_id, void *event_data)
+{
+    ip_event_got_ip6_t *event = (ip_event_got_ip6_t *)event_data;
+    memcpy(&s_ipv6_addr, &event->ip6_info.ip, sizeof(s_ipv6_addr));
+    xEventGroupSetBits(s_connect_event_group, GOT_IPV6_BIT);
+}
+
+#endif // CONFIG_EXAMPLE_CONNECT_IPV6
+
+esp_err_t example_connect()
+{
+    if (s_connect_event_group != NULL) {
+        return ESP_ERR_INVALID_STATE;
+    }
+    s_connect_event_group = xEventGroupCreate();
+    start();
+    xEventGroupWaitBits(s_connect_event_group, CONNECTED_BITS, true, true, portMAX_DELAY);
+    ESP_LOGI(TAG, "Connected to %s", s_connection_name);
+    ESP_LOGI(TAG, "IPv4 address: " IPSTR, IP2STR(&s_ip_addr));
+#ifdef CONFIG_EXAMPLE_CONNECT_IPV6
+    ESP_LOGI(TAG, "IPv6 address: " IPV6STR, IPV62STR(s_ipv6_addr));
+#endif
+    return ESP_OK;
+}
+
+esp_err_t example_disconnect()
+{
+    if (s_connect_event_group == NULL) {
+        return ESP_ERR_INVALID_STATE;
+    }
+    vEventGroupDelete(s_connect_event_group);
+    s_connect_event_group = NULL;
+    stop();
+    ESP_LOGI(TAG, "Disconnected from %s", s_connection_name);
+    s_connection_name = NULL;
+    return ESP_OK;
+}
+
+#ifdef CONFIG_EXAMPLE_CONNECT_WIFI
+
+    static void on_wifi_disconnect(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data)
+    {   
+        ESP_LOGI(TAG, "Wi-Fi disconnected, trying to reconnect...");
+        ESP_ERROR_CHECK(esp_wifi_connect());
+    }   
+
+#ifdef CONFIG_EXAMPLE_CONNECT_IPV6
+
+    static void on_wifi_connect(void *arg, esp_event_base_t event_base,
+                            int32_t event_id, void *event_data)
+    {
+        tcpip_adapter_create_ip6_linklocal(TCPIP_ADAPTER_IF_STA);
+    }
+
+#endif // CONFIG_EXAMPLE_CONNECT_IPV6
+
+static void start()
+{
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_got_ip, NULL));
+#ifdef CONFIG_EXAMPLE_CONNECT_IPV6
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, &on_wifi_connect, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_GOT_IP6, &on_got_ipv6, NULL));
+#endif
+
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = ssid,
+            .password = pass,
+        },
+    };
+    ESP_LOGI(TAG, "Connecting to %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_connect());
+    s_connection_name = CONFIG_EXAMPLE_WIFI_SSID;
+}
+
+static void stop()
+{
+    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &on_wifi_disconnect));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &on_got_ip));
+#ifdef CONFIG_EXAMPLE_CONNECT_IPV6
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_GOT_IP6, &on_got_ipv6));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, WIFI_EVENT_STA_CONNECTED, &on_wifi_connect));
+#endif
+    ESP_ERROR_CHECK(esp_wifi_stop());
+    ESP_ERROR_CHECK(esp_wifi_deinit());
+}
+#endif // CONFIG_EXAMPLE_CONNECT_WIFI
+
+#ifdef CONFIG_EXAMPLE_CONNECT_ETHERNET
+
+#ifdef CONFIG_EXAMPLE_CONNECT_IPV6
+
+/** Event handler for Ethernet events */
+static void on_eth_event(void *arg, esp_event_base_t event_base,
+                         int32_t event_id, void *event_data)
+{
+    switch (event_id) {
+    case ETHERNET_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "Ethernet Link Up");
+        tcpip_adapter_create_ip6_linklocal(TCPIP_ADAPTER_IF_ETH);
+        break;
+    default:
+        break;
+    }
+}
+
+#endif // CONFIG_EXAMPLE_CONNECT_IPV6
+
+static esp_eth_handle_t s_eth_handle = NULL;
+static esp_eth_mac_t *s_mac = NULL;
+static esp_eth_phy_t *s_phy = NULL;
+
+static void start()
+{
+    ESP_ERROR_CHECK(tcpip_adapter_set_default_eth_handlers());
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &on_got_ip, NULL));
+#ifdef CONFIG_EXAMPLE_CONNECT_IPV6
+    ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ETHERNET_EVENT_CONNECTED, &on_eth_event, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_GOT_IP6, &on_got_ipv6, NULL));
+#endif
+    eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
+    s_mac = esp_eth_mac_new_esp32(&mac_config);
+    eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
+#if CONFIG_EXAMPLE_ETH_PHY_IP101
+    s_phy = esp_eth_phy_new_ip101(&phy_config);
+#elif CONFIG_EXAMPLE_ETH_PHY_RTL8201
+    s_phy = esp_eth_phy_new_rtl8201(&phy_config);
+#elif CONFIG_EXAMPLE_ETH_PHY_LAN8720
+    s_phy = esp_eth_phy_new_lan8720(&phy_config);
+#elif CONFIG_EXAMPLE_ETH_PHY_DP83848
+    s_phy = esp_eth_phy_new_dp83848(&phy_config);
+#endif
+    esp_eth_config_t config = ETH_DEFAULT_CONFIG(s_mac, s_phy);
+
+    ESP_ERROR_CHECK(esp_eth_driver_install(&config, &s_eth_handle));
+    s_connection_name = "Ethernet";
+}
+
+static void stop()
+{
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_ETH_GOT_IP, &on_got_ip));
+#ifdef CONFIG_EXAMPLE_CONNECT_IPV6
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_GOT_IP6, &on_got_ipv6));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(ETH_EVENT, ETHERNET_EVENT_CONNECTED, &on_eth_event));
+#endif
+    ESP_ERROR_CHECK(esp_eth_driver_uninstall(s_eth_handle));
+    ESP_ERROR_CHECK(s_phy->del(s_phy));
+    ESP_ERROR_CHECK(s_mac->del(s_mac));
+}
+
+#endif // CONFIG_EXAMPLE_CONNECT_ETHERNET
 
 static esp_err_t event_handler(void *ctx, system_event_t *event)
 {
@@ -116,15 +311,15 @@ static esp_err_t home_handler(httpd_req_t *req)
         if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
             char value[2];
             httpd_query_key_value(buf, "start", (char *) &value, 2);
-            value = atoi(value);
-            if (value == 3) {
+            changed_value = atoi(value);
+            if (changed_value == 3) {
                 servo_init(robot);
             }
             else {
-                robot->stop = value;
+                robot->stop = changed_value;
             }
             
-            robot->stop = value;
+            robot->stop = changed_value;
             ESP_LOGE(TAG, "Received stop command: %d", robot->stop);
         }
         free(buf);
@@ -175,50 +370,6 @@ static httpd_handle_t start_webserver(void)
     return NULL;
 }
 
-/*
-    AP Mode
-*/
-
-static void initialise_wifi(void)
-{
-    esp_log_level_set("wifi", ESP_LOG_WARN);
-    static bool initialized = false;
-    if (initialized) {
-        return;
-    }
-    tcpip_adapter_init();
-    wifi_event_group = xEventGroupCreate();
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_NULL) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-    initialized = true;
-}
-
-void wifi_init_softap()
-{
-    initialise_wifi();
-    wifi_config_t wifi_config = {
-        .ap = {
-            .ssid = ESP_WIFI_SSID,
-            .ssid_len = strlen(ESP_WIFI_SSID),
-            .password = ESP_WIFI_PASS,
-            .max_connection = MAX_STA_CONN,
-            .authmode = WIFI_AUTH_WPA_WPA2_PSK
-        },
-    };
-    //wifi_config.ap.authmode = WIFI_AUTH_OPEN;
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s\n", ESP_WIFI_SSID, ESP_WIFI_PASS);
-}
-
 // ==================================================================================================
 //          Start http server for wifi config setting
 // ==================================================================================================
@@ -226,6 +377,27 @@ void start_http_server(robot_t * robot_)
 {
     robot = robot_;
 
-    wifi_init_softap();
-    start_webserver();
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    /* Register event handlers to stop the server when Wi-Fi or Ethernet is disconnected,
+     * and re-start it upon connection.
+     */
+    #ifdef CONFIG_EXAMPLE_CONNECT_WIFI
+        ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &connect_handler, &server));
+        ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
+    #endif // CONFIG_EXAMPLE_CONNECT_WIFI
+    #ifdef CONFIG_EXAMPLE_CONNECT_ETHERNET
+        ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &connect_handler, &server));
+        ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ETHERNET_EVENT_DISCONNECTED, &disconnect_handler, &server));
+    #endif // CONFIG_EXAMPLE_CONNECT_ETHERNET
+
+    /* Start the server for the first time */
+    server = start_webserver();
 }
